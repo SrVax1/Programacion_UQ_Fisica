13a022776ae9325e78fbf413e347c9335303263f
Jhon Sebastian Garcia <jhons.garciab@uqvirtual.edu.co>
2023-03-02 22:54:37 -0500

    Tarea condicionales

---
+++ Tarea.py
@@ -0,0 +1,23 @@
+edad = int(input("Ingrese su edad: "))
+mes = int(input("Ingrese el mes de su cumpleaños (en número): "))
+
+if edad <= 0:
+    print("Sea serio")
+    edad = int(input("Ingrese su edad nuevamente: "))
+    mes = input("Ingrese el mes de su cumpleaños (en número): ")
+    if edad > 0 and edad < 18:
+        print("Es usted menor de edad")
+    elif edad > 18 or (edad == 18 and int(mes) <= 2):
+        print("Es usted mayor de edad")
+    else:
+        print("Es usted menor de edad")
+else:
+    if edad > 18 or (edad == 18 and int(mes) <= 2):
+        print("Es usted mayor de edad")
+    else:
+        print("Es usted menor de edad")
+
+
+
+
+
